d4a9d630e74e762d8f9dfbfb5853cc73fdf58b4b
Jamie Arkin <jamielauren.arkin@gmail.com>
2020-09-30 20:30:45 -0600

    finish part 3

---
+++ part-3/src/components/Form.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/grid";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  btn: {
+    margin: theme.spacing(1),
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: "100%",
+  },
+}));
+
+const Form = (props) => {
+  const { seed_kits, gardens } = props.data;
+  const classes = useStyles();
+  const [userSeedKit, setUserSeedKit] = React.useState("");
+  const [userGardens, setUserGardens] = React.useState([]);
+
+  let allGardens = Object.keys(gardens).map((garden) => {
+    return gardens[garden];
+  });
+
+  const handleChange = (event) => {
+    const selectedKit = event.target.value;
+    const kit = seed_kits[selectedKit];
+
+    let updatedGardens = allGardens.filter((garden) => {
+      const minPods = Math.min.apply(null, kit.pods);
+      return (
+        garden.wattage >= kit.min_wattage &&
+        garden.grow_height >= kit.min_grow_height &&
+        garden.pods >= minPods
+      );
+    });
+
+    setUserGardens([...updatedGardens]);
+    setUserSeedKit(selectedKit);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3} justify="center">
+        <Grid item xs={12} md={6} lg={4}>
+          <FormControl className={classes.formControl}>
+            <InputLabel>What type of plant would you like to grow?</InputLabel>
+            <Select onChange={handleChange} value={userSeedKit}>
+              {Object.keys(seed_kits).map((kit, index) => {
+                return (
+                  <MenuItem key={index} value={kit}>
+                    {seed_kits[kit].name}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+          {userSeedKit && (
+            <>
+              {userGardens.map((garden, index) => {
+                return (
+                  <Button
+                    key={index}
+                    className={classes.btn}
+                    variant="contained"
+                    color="primary"
+                  >
+                    {garden.name}
+                  </Button>
+                );
+              })}
+            </>
+          )}
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Form;
